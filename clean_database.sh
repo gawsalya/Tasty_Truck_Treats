142921bc48e1ea04e85cb55ca56f4a824673f75f
gawsalya <110790958+gawsalya@users.noreply.github.com>
2023-08-13 22:20:31 +0100

    Delete clean_database.sh

--- clean_database.sh
+++
@@ -1,6 +0,0 @@
-source .env
-echo "clean up initiated"
-export PGPASSWORD=$PASSWORD
-psql --host $HOST --port $PORT -U $DB_USER $DB_NAME -c "DROP TABLE sigma_gawsalya_schema.dim_date, sigma_gawsalya_schema.dim_truck, sigma_gawsalya_schema.dim_type, sigma_gawsalya_schema.fact_truck_transaction CASCADE;"
-psql --host $HOST --port $PORT -U $DB_USER $DB_NAME -c "\q"
-echo "clean up complete"
